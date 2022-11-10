b78a0908ab8497a9634613aa5ffe209f91476f77
kdsrivastav <58316558+kdsrivastav@users.noreply.github.com>
2022-11-10 08:16:53 +0530

    Add files via upload

---
+++ cars_sell/welcome.php
@@ -0,0 +1,43 @@
+
+<html">
+   
+   <head>
+      <title>Welcome </title>
+      <style type="text/css">
+         body {
+            background-image: linear-gradient(to right top, #051937, #004d7a, #008793, #00bf72, #a8eb12);
+         }
+         .container {
+            max-width: 500px;
+            margin: auto;
+            background-color: rgba(255, 255, 255, 0.3);
+            padding: 10px;
+            margin-top: 20%;
+         }
+         .container h1 {
+            text-align: center;
+            color: rgba(220, 220, 220, 1.0);
+         }
+         .container h2 {
+            text-align: center;
+         }
+         .container h2 a {
+            text-decoration: none;
+            color: rgba(180, 180, 180, 1.0);
+         }
+         .container h2 a:hover {
+            color: rgba(220, 220, 220, 1.0);
+         }
+
+
+      </style>
+   </head>
+   
+   <body>
+      <div class="container">
+         <h1>Login Successful.</h1> 
+      <h2><a href = "logout.php">Sign Out</a></h2>
+      </div>
+   </body>
+   
+</html>
